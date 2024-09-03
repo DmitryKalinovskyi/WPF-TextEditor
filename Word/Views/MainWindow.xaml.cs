0c341e09ecbd7e2ddfc44e6d3e55abc0743f5551
Dmytro <ipz223_kdm@student.ztu.edu.ua>
2024-09-04 00:07:20 +0300

    Added logic to the main window buttons

--- Word/Views/MainWindow.xaml.cs
+++ Word/Views/MainWindow.xaml.cs
@@ -10,18 +10,18 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Wpf.Ui.Controls;
 
 namespace Word
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : FluentWindow
+    public partial class MainWindow : Wpf.Ui.Controls.FluentWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            _fontSize.SelectedValue = _richTextBox.FontSize;
         }
 
         private void TabItem_PreviewMouseDown(object sender, MouseButtonEventArgs e)
@@ -32,6 +32,37 @@ namespace Word
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             ContextMenu.IsOpen = true;
+        }
+
+        private void _fontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                object? fontSize = e.AddedItems[0];
+                if(fontSize != null)
+                ApplyPropertyToSelectedText(FontSizeProperty, fontSize);
+            }
+            catch (Exception ex) { }
+
+            _richTextBox.Focus();
+        }
+
+        private void ApplyPropertyToSelectedText(DependencyProperty dependencyProperty, object? value)
+        {
+            _richTextBox.Selection.ApplyPropertyValue(dependencyProperty, value);
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                FontFamily? editValue = (FontFamily?)e.AddedItems[0];
+                if(editValue != null) 
+                    ApplyPropertyToSelectedText(FontFamilyProperty, editValue);
+            }
+            catch (Exception ex) { }
+
+            _richTextBox.Focus();
         }
     }
 }
